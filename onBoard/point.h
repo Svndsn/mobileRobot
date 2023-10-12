1863ba8af20a46782de189046b4da02104c17bb1
SimonLinux <simon.egeris.svendsen@gmail.com>
2023-10-12 12:07:21 +0200

    Update so the point cant change, but the robot can

--- onBoard/point.h
+++ onBoard/point.h
@@ -4,13 +4,16 @@ class point
 {
 private:
     float x, y;
+    void setX(float);
+    void setY(float);
 public:
     point(float _x, float _y);
     ~point();
     float distanceBetweenPoints(point b); // Calculates sortest distance between two points
-    //Getters and setters
+    //Getters
     float getX();
     float getY();
-    void setX(float x);
-    void setY(float y);
+
+
+    friend class robot; //makes the robot class able to set and change X and Y without the point being able to do that
 };
